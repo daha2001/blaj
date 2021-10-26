cd19b8385939cf6445ce95b685042312ab09089c
Daniel Håkansson <danielhakansson2@gmail.com>
2021-10-26 14:13:24 +0200

    Created a .gitignore and test project

---
+++ frontend-tests/cypress/integration/regression-test-suite.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+
+// This is a test suite
+describe('regresseion test suite', function(){
+
+    // This is a test case
+    it('perform valid login', function(){
+        cy.visit('http://localhost:3000')
+
+    })
+
+
+})
